e322fa6866b12114da03b1c3ff8634b6d0ad0c30
Ailrk <jimmy123good@hotmail.com>
2021-09-28 23:46:13 -0700

    handle

--- haskell_netpbm-handle/src/NetPBM/Format.hs
+++ haskell_netpbm-handle/src/NetPBM/Format.hs
@@ -9,14 +9,16 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
 module NetPBM.Format
   ( SerializeFormat(..)
   , IsPxiel(..)
-  , Format
-  , Magic
+  , Format(..)
+  , Magic(..)
+  , withFormat
   ) where
 
 import qualified Data.ByteString    as BS
@@ -34,51 +36,62 @@ data Magic = P1 | P2 | P3 | P4 | P5 | P6 | P7 deriving Show
 
 -------------------------------------------------------------------------------
 
-type family HeaderRepDefault (a :: Format) (s :: Magic) :: Type where
-  HeaderRepDefault PBM P4 = SimpleHeader
-  HeaderRepDefault PBM P1 = SimpleHeader
-  HeaderRepDefault PGM P2 = SimpleHeader
-  HeaderRepDefault PGM P5 = SimpleHeader
-  HeaderRepDefault PPM P3 = SimpleHeader
-  HeaderRepDefault PPM P6 = SimpleHeader
+type family Magic2FormatBin (s :: Magic) :: Format where
+  Magic2FormatBin P4 = PBM
+  Magic2FormatBin P2 = PGM
+  Magic2FormatBin P6 = PPM
 
-type family PixelRepDefault (a :: Format) (s :: Magic) :: Type where
-  PixelRepDefault a s = PixelRep a s
+type family Magic2FormatSimple (s :: Magic) :: Format where
+  Magic2FormatSimple P1 = PBM
+  Magic2FormatSimple P5 = PGM
+  Magic2FormatSimple P3 = PPM
 
-class SerializeFormat (a :: Format) (s :: Magic) where
-  type PixelRep a s
+type family HeaderRepDefault (s :: Magic) :: Type where
+  HeaderRepDefault P4 = SimpleHeader
+  HeaderRepDefault P1 = SimpleHeader
+  HeaderRepDefault P2 = SimpleHeader
+  HeaderRepDefault P5 = SimpleHeader
+  HeaderRepDefault P3 = SimpleHeader
+  HeaderRepDefault P6 = SimpleHeader
 
-  type HeaderRep (a :: Format) (s :: Magic) :: Type
-  type HeaderRep a s = HeaderRepDefault a s
+type family PixelRepDefault (s :: Magic) :: Type where
+  PixelRepDefault s = PixelRep s
 
-  header :: (r ~ HeaderRep a s) => r
+class SerializeFormat (s :: Magic) where
+  type PixelRep s
+
+  type HeaderRep (s :: Magic) :: Type
+  type HeaderRep s = HeaderRepDefault s
+
+  header :: (r ~ HeaderRep s) => r
   default header :: (r ~ SimpleHeader) => r
   header = simpleHeader
 
-  serialize :: forall rep . (rep ~ (PixelRep a s), IsPxiel rep)
+  serialize :: forall rep
+             . (rep ~ (PixelRep s), IsPxiel rep)
             => BS.ByteString -> [rep] -> BS.ByteString
-  serialize h xs = BS.concat (h : fmap toByte xs)
+  serialize h xs = BS.concat (" " : h : fmap toByte xs)
 
 -- monochrome format
-instance SerializeFormat PBM P4 where
-  type PixelRep PBM P4 = Word8
+instance SerializeFormat P4 where
+  type PixelRep P4 = Word8
 
-instance SerializeFormat PBM P1 where
-  type PixelRep PBM P1 = Char
+instance SerializeFormat P1 where
+  type PixelRep P1 = Char
 
 -- grayscale format
-instance SerializeFormat PGM P5 where
-  type PixelRep PGM P5 = Word8
+instance SerializeFormat P5 where
+  type PixelRep P5 = Word8
 
-instance SerializeFormat PGM P2 where
-  type PixelRep PGM P2 = T.Text
+instance SerializeFormat P2 where
+  type PixelRep P2 = T.Text
 
 -- color image format
-instance SerializeFormat PPM P6 where
-  type PixelRep PPM P6 = (Word8, Word8, Word8)
+instance SerializeFormat P6 where
+  type PixelRep P6 = (Word8, Word8, Word8)
 
-instance SerializeFormat PPM P3 where
-  type PixelRep PPM P3 = (T.Text, T.Text, T.Text)
+instance SerializeFormat P3 where
+  type PixelRep P3 = (T.Text, T.Text, T.Text)
 
 
 -- header with w h m format
@@ -87,7 +100,6 @@ simpleHeader :: SimpleHeader
 simpleHeader width height maxval =
       BS.pack . fmap (fromIntegral . ord)
     $ mconcat [show width, " ", show height, " ", show maxval, " "]
-
 
 -------------------------------------------------------------------------------
 
@@ -115,8 +127,21 @@ instance IsPxiel (T.Text, T.Text, T.Text) where
   defaultPixelRep = ("0", "0", "0")
   toByte (a, b, c) = BS.concat [toByte a, toByte b, toByte c]
 
-withFormat :: forall (s :: Format) (a :: Magic) w . w
-withFormat = undefined
+-- goal?
+-- 1. header is polymorphic,
+-- 2. we need to apply types for both serialize and header.
+-- 3. we want to unify these two funcion in one.
+
+withFormat :: (SerializeFormat m
+                 , rep ~ PixelRep m
+                 , IsPxiel rep
+                 , h ~ HeaderRep m
+                 , f ~ (BS.ByteString -> [rep] -> BS.ByteString))
+              => Proxy m
+              -> (h -> f -> BS.ByteString)
+              -> BS.ByteString
+withFormat (p :: Proxy m) k = k (header @m) (serialize @m)
+
 
 -- b = header @PPM @P6 10 20 30
 -- c = serialize @PPM @P6 (header @PPM @P6 1 20 30) []
--- haskell_netpbm-handle/test/Main.hs
+++ haskell_netpbm-handle/test/Main.hs
@@ -6,9 +6,11 @@ module Main where
 
 import           Control.Monad.ST
 import           Data.Char
+import           Data.Proxy
 import           Data.Word
 import           Debug.Trace
 import qualified NetPBM.Canvas    as C
+import qualified NetPBM.Format    as F
 import           Test.Hspec
 
 main :: IO ()
@@ -26,3 +28,14 @@ test_canvas = do
         (Just c) <- C.index (m, n) v
         C.modify v (m, n) (+ (fromIntegral (ord 'a')))
       return v
+
+test_format :: IO ()
+test_format = do
+  let b = withFormat (Proxy @P5) $ \header serialize ->
+    let data = [ ' ', ' ', 'z'
+               , ' ', 'z', ' '
+               , 'z', ' ', ' ']
+        wdata = (fromIntegral . ord) data
+     in serialize (header 3 3 255) wdata
+  putStrLn (show b)
+  putStrLn "hi"
