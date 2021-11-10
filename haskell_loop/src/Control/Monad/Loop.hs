1d27faad6c01fbb3b0716c34449bf95a20d3a47c
Ailrk <jimmy123good@hotmail.com>
2021-11-09 19:51:53 -0800

    loop while

--- haskell_loop/src/Control/Monad/Loop.hs
+++ haskell_loop/src/Control/Monad/Loop.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts         #-}
 {-# LANGUAGE FlexibleInstances        #-}
 {-# LANGUAGE FunctionalDependencies   #-}
 {-# LANGUAGE GADTs                    #-}
@@ -5,46 +6,42 @@
 {-# LANGUAGE RankNTypes               #-}
 {-# LANGUAGE ScopedTypeVariables      #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TupleSections            #-}
+{-# LANGUAGE TypeFamilies             #-}
 module Control.Monad.Loop where
 
 
 import           Control.Concurrent
-import           Control.Exception
 import           Control.Monad
-import           Data.Kind          (Type)
-import Data.Foldable
+import           Control.Monad.Except
+import           Control.Monad.ST.Lazy
+import           Control.Monad.State
+import           Data.Foldable
+import           Data.Kind            (Type)
+import           Data.Maybe
+import           Data.STRef.Lazy
+import           Debug.Trace
 
--- cases
--- from i to j        -> build [i, j..1]
--- from i to j step k -> build [i,j..k]
--- across xs          -> build [a0, a1, ..]
--- while              -> give [i, j..k], put in except monad, check
---                       before calling action, if unsatisfied then break.
-
--- | List wrapper for from .. to .. step loop. This type avoid conflit with
--- | general traversables.
-newtype IndexList n = IndexList [n]
 
 type Loop :: (Type -> Type) -> Type -> Type -> Type
-data Loop m r a where
-  Across :: Traversable f => Loop m r a -> f a -> Loop m r (f a)
-  While  :: Traversable f
-         => Loop m r (f a) -> (a -> Bool) -> Loop m r (f a, a -> Bool)
-  Run    :: Loop m r a
 
-across :: Traversable f => Loop m r a -> f a -> Loop m r (f a)
+data Loop m r a where
+  Across :: Traversable t => Loop m r () -> t a -> Loop m r (t a)
+  While  :: Traversable t
+         => Loop m r (t a) -> (a -> Bool) -> Loop m r (t a, a -> Bool)
+  Run    :: Loop m r ()
+
+across :: Traversable t => Loop m r () -> t a -> Loop m r (t a)
 across = Across
 
-while  :: Traversable f
-       => Loop m r (f a) -> (a -> Bool) -> Loop m r (f a, a -> Bool)
+while  :: Traversable t
+       => Loop m r (t a) -> (a -> Bool) -> Loop m r (t a, a -> Bool)
 while = While
 
 loop = Run
 
 evalLoop :: Monad m => Loop m r a -> m a
-evalLoop Run = undefined
+evalLoop Run = return ()
 evalLoop (Across loop xs) = do
   _ <- evalLoop loop
   return xs
@@ -53,32 +50,40 @@ evalLoop (While loop pred) = do
   xs <- evalLoop loop
   return (xs, pred)
 
+with_ :: (Traversable t, Monad m)
+      => Loop m r (t a) -> (a -> ExceptT () m ()) -> m ()
+with_ loop k = do
+  xs <- evalLoop loop
+  runExceptT $ traverse_ k xs
+  return ()
 
-class With loop k where
-  type WithRet loop k
-  with :: loop -> k -> WithRet loop k
+enumerateTrav :: (Traversable t, Integral n) => t a -> t (n, a)
+enumerateTrav ts = runST $ do
+  idxref <- newSTRef 0
+  flip traverse ts $ \value -> do
+    idx <- readSTRef idxref
+    idxref `modifySTRef` (+ 1)
+    return (idx, value)
 
-instance (Traversable f, Monad m) =>
-         With (Loop m r (f a)) (a -> m r)
-  where
-  type WithRet (Loop m r (f a)) (a -> m r) = m (f r)
-  with loop k = do
-    xs <- evalLoop loop
-    traverse k xs
+withi_ :: (Traversable t, Monad m, Integral n)
+       => Loop m r (t a) -> ((n, a) -> ExceptT () m ()) -> m ()
+withi_ loop k = do
+  xs <- evalLoop loop
+  runExceptT $ traverse_ (\n -> trace "hi" $ k n) (enumerateTrav xs)
+  return ()
 
-instance (Traversable f, Monad m) =>
-         With (Loop m r (f a)) (a -> m ())
-  where
-  type WithRet (Loop m r (f a)) (a -> m ()) = m ()
--- with_ :: (Traversable f, Monad m) => Loop m r (f a) -> (a -> m r) -> m ()
-  with loop k = do
-    xs <- evalLoop loop
-    traverse_ k xs
-
-
-
+withWhile_ :: (Traversable t, Monad m)
+           => Loop m r (t a, a -> Bool) -> (a -> ExceptT () m ()) -> m ()
+withWhile_ loop k = do
+  (ts, pred) <- evalLoop loop
+  runExceptT $ traverse_ (\a -> if pred a then k a else throwError ()) ts
+  return ()
 
 
--- foo :: IO ()
--- foo = do
---   loop `across` [1, 2, 3] `with_` \i -> return ()
+-- | wrap monadic action in ExceptT that we can jump out.
+loopWrap :: Monad m => ExceptT e m a -> m e
+loopWrap = fmap (either id id) . runExceptT . forever
+
+-- | break to the outer loop.
+quit :: Monad m => ExceptT () m a
+quit = throwError ()
